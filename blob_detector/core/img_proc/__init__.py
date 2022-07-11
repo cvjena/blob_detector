747d2d985ed0d4389185bf89945d9398f51719c9
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2022-07-11 10:52:00 +0200

    renamed border operations module

--- blob_detector/core/img_proc/__init__.py
+++ blob_detector/core/img_proc/__init__.py
@@ -1,4 +1,4 @@
-from blob_detector.core.img_proc.border_removal import BorderRemoval
+from blob_detector.core.img_proc.border_ops import BorderRemoval
 from blob_detector.core.img_proc.morphological import MorphologicalOps
 from blob_detector.core.img_proc.preprocessor import Preprocessor
 from blob_detector.core.img_proc.rescaler import Rescaler

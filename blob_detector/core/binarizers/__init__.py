ffac9b0843a60fc05fa590ae56db08c44d3386e2
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2022-07-13 10:06:09 +0200

    added local version of the otsu thresholding

--- blob_detector/core/binarizers/__init__.py
+++ blob_detector/core/binarizers/__init__.py
@@ -2,6 +2,7 @@ import enum
 
 from blob_detector.core.binarizers.gauss_local import GaussLocalTresholder
 from blob_detector.core.binarizers.high_pass import HighPassTresholder
+from blob_detector.core.binarizers.otsu import LocalOtsuTresholder
 from blob_detector.core.binarizers.otsu import OtsuTresholder
 
 class BinarizerType(enum.Enum):
@@ -9,6 +10,7 @@ class BinarizerType(enum.Enum):
     gauss_local = GaussLocalTresholder
     high_pass = HighPassTresholder
     otsu = OtsuTresholder
+    otsu_local = LocalOtsuTresholder
 
 
 def new(type: BinarizerType, *args, **kwargs):
--- blob_detector/core/binarizers/otsu.py
+++ blob_detector/core/binarizers/otsu.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 
 from skimage import filters
+from skimage.filters import rank
+from skimage.filters import threshold_otsu
+from skimage.morphology import disk
 
 from blob_detector import core
 from blob_detector.core.binarizers import base
@@ -15,9 +18,28 @@ class OtsuTresholder(base.BaseThresholder):
     def threshold(self, X: core.ImageWrapper) -> base.ThreshReturn:
         im = X.im
         if self._use_cv2:
-            thresh, bin_im = cv2.threshold(im, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            thresh, bin_im = cv2.threshold(im, 0, 255,
+                cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
         else:
         	thresh = filters.threshold_otsu(im)
         	bin_im = None
 
         return base.ThreshReturn(thresh=thresh, bin_im=bin_im)
+
+class LocalOtsuTresholder(base.BaseThresholder):
+
+    def __init__(self, *, window_size: int = 30, **kwargs):
+        super().__init__(**kwargs)
+        self.window_size = window_size
+
+    def threshold(self, X: core.ImageWrapper) -> base.ThreshReturn:
+        mask = X.mask if self._use_masked else None
+        im = X.im
+        footprint = disk(self.window_size)
+        thresh = filters.rank.otsu(im, footprint, mask=mask)
+
+        bin_im = im >= thresh
+        bin_im = (bin_im * 255).astype(np.uint8)
+
+        return base.ThreshReturn(thresh=0, bin_im=bin_im)
